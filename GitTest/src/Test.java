0435c49e58c39e7159489a208492374a516a4f15
Octavio <octavio.berlanga@aa.com>
2017-02-27 13:05:44 -0600

    Added hashcode, equals, toString methods

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -9,4 +9,34 @@ public class Test {
 		this.subject = subject;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Test other = (Test) obj;
+		if (subject == null) {
+			if (other.subject != null)
+				return false;
+		} else if (!subject.equals(other.subject))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Test [subject=" + subject + "]";
+	}
+
 }
